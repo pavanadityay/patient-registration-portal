4f010df55387ddf841f1d416fd2f02aa22cec415
Pavan Aditya Rao <p.adithya17@gmail.com>
2026-06-14 19:45:49 -0300

    Updated changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","http://20.151.240.218"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
